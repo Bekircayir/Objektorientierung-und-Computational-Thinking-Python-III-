35037bd2a839b817ffaf72f47cb77a1e0bfff494
Bekir Cayir <62176320+Bekircayir@users.noreply.github.com>
2022-09-29 11:37:10 +0200

    Delete pandas directory

--- pandas/countries.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('countries.csv')
-
-print(df["GDP: Gross domestic product (million current US$)"].mean())
--- pandas/temperatures.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('TemperatureData.csv')
-plt.scatter(df["Date"], df["CRU3 SH"])
-plt.show()
