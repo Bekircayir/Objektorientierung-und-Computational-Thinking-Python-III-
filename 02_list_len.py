ed81e626a88b349314fcf79fe5c9eb16afb2830a
Bekir Cayir <62176320+Bekircayir@users.noreply.github.com>
2022-09-29 11:38:41 +0200

    Delete 02_list_len.py

--- 02_list_len.py
+++
@@ -1,8 +0,0 @@
-def recursiveSum(int_list):
-    if len(int_list) == 1:
-        return int_list[0]
-    else:
-        return int_list[0] + recursiveSum(int_list[1:])
-
-
-print(recursiveSum([3, 8, 5, 26, 7]))
